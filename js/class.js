c9f9b4fc33e0899c2c94c8ae90b847ddaa7a50ad
Morito Sato <huroppi.tsukuba@gmail.com>
2018-08-25 21:22:13 +0900

    add clock.dwarDelayRing()

--- js/class.js
+++ js/class.js
@@ -10,13 +10,16 @@ class Clock {
     /* for time controlling */
     this.time = 60.;
     this.delayTime = 2.;
+    this.delayTimeLeft = 0.1;
+    this.timeLeft = 30.4;
   }
-/test
+
   run(){
     push();
     translate(this.x0, this.y0);
     //this.shake();
     this.drawBaseRing();
+    this.drawDelayRing();
     pop();
   }
 
@@ -30,9 +33,38 @@ class Clock {
     pop();
   }
 
-  drawDelayRing(){
-    if (this.delayTime >= 0) { //when delay time left
+  drawTensionCircle(){
+    let percent = this.timeLeft / this.Time;
+    let circlePercent = map(percent, 0, 1, 1, 0);
+    let pulse = this.timeLeft % 1;
 
+    draw
+
+  }
+
+  drawDelayRing(){
+    if (this.delayTimeLeft >= 0) { //when delay time left
+      let percent = this.delayTimeLeft / this.delayTime
+      let angle = map(percent, 0, 1, 0, 2*PI);
+
+      for (let i = 0; i <= angle; i+=.02){
+        push();
+        fill(33, 175, 255);//blue
+        noStroke();
+        let x = cos(-(i + (PI / 2))) * this.sizeClock / 2.;
+        let y = sin(-(i + (PI / 2))) * this.sizeClock / 2.;
+        ellipse(x, y, 12);
+
+        //Edge of Ring
+        if (abs(i - angle) < 0.02){
+          ellipse(x, y, 21);
+        }
+        pop();
+      }
+
+      this.delayTimeLeft -= 0.01;//for debug
+    } else{
+      this.delayTimeLeft = 2.0;//for debug
     }
   }
 
