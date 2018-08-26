8f23786b5dc70a63bf7d8a733439a09b2c131006
Morito Sato <huroppi.tsukuba@gmail.com>
2018-08-26 11:31:59 +0900

    add Counter

---
+++ js/counter.js
@@ -0,0 +1,31 @@
+class Counter {
+  constructor(){
+    this.count = 12;
+  }
+
+  run(){
+    this.drawCount();
+  }
+
+  drawCount(){
+    push();
+    stroke(33, 175, 255);//blue
+    //stroke('black');//blue
+    //strokeWeight(3.);
+    textSize(50);
+    textStyle(NORMAL);
+    textAlign(CENTER, CENTER);
+    //fill(33, 175, 255);
+    fill('white');
+    text(str(this.count), 0, -height / 3.5);
+    pop();
+  }
+
+  increCount(){
+    this.count += 1;
+  }
+
+  resetVars(){
+    this.count = 0;
+  }
+}
--- js/main.js
+++ js/main.js
@@ -5,11 +5,13 @@ const FRAME_RATE = 60;
 
 function setup(){
   fullscreen();
+  //createCanvas(windowWidth, windowHeight);//描画領域を指定
   createCanvas(800, 800 * 0.5625);//描画領域を指定
   frameRate(FRAME_RATE);
-  clock_left = new Clock(-(width / 3.95), height / 15);
-  clock_right = new Clock(+(width / 3.95), height / 15);
+  clock_left = new Clock(-(width / 3.95), height / 15, 60., 2.);
+  clock_right = new Clock(+(width / 3.95), height / 15, 60., 2.);
   engine = new Engine();
+  counter = new Counter();
 }
 
 function draw(){
@@ -20,12 +22,19 @@ function draw(){
   translate(width / 2., height / 2.);
   clock_left.run();
   clock_right.run();
+  counter.run();
 }
 
 function keyPressed(){
   if (key === 'u'){
     clock_left.resetVars();
     clock_right.resetVars();
+    clock_left.startStop();
+  }
+
+  if (key == 's'){
+    clock_left.startStop();
+    clock_right.startStop();
   }
 
   return false;
